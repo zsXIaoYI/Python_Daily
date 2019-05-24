576bf23183446aa25e5548edf0bda2feb5c8a308
zhangsong01 <zhangsong01@missfresh.cn>
2019-05-24 20:56:57 +0800

    类的另一种创建形式

---
+++ cn/zsza/oop/_another_class.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# __author__ = 'zs'
+# 2019/5/24 下午8:47
+
+
+# 类的另一种创建形式
+
+def bar(self, name):
+    self._name = name
+
+
+def foo(self, course_name):
+    print('%s正在学习%s.' % (self._name, course_name))
+
+
+def main():
+    Student = type('Student', (object,), dict(__init__=bar, study=foo))
+    stu1 = Student('Amy')
+    stu1.study('Python程序设计')
+
+
+if __name__ == '__main__':
+    main()

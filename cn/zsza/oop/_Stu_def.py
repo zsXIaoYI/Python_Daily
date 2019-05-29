7b12eceb0130b8802c4a3c78abf31de7589f46de
zhangsong01 <zhangsong01@missfresh.cn>
2019-05-29 21:47:35 +0800

    新增类方法和静态方法

--- cn/zsza/oop/_Stu_def.py
+++ cn/zsza/oop/_Stu_def.py
@@ -14,3 +14,26 @@ class Student(object):
 
     def get_name(self):
         return self.__name
+
+
+class A(object):
+
+    def m1(self, n):
+        print('self:', self)
+
+    @classmethod
+    def m2(cls):
+        print('cls:', cls)
+
+    @staticmethod
+    def m3(num):
+        print('num:', num)
+
+
+if __name__ == '__main__':
+    a = A()
+    a.m1(2)
+
+    A.m2()
+    A.m3(66)
+

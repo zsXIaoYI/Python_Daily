a1a64232be7e6076ff7c7b22dbed1ba90d174f72
zhangsong01 <zhangsong01@missfresh.cn>
2019-06-20 17:11:28 +0800

    新增_2.py

---
+++ cn/zsza/io/_2.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# __author__ = 'zs'
+# 2019/6/20 下午4:54
+
+
+import time
+
+
+def main():
+    # 一次性读取整个文件的内容
+    with open('1.txt', 'r', encoding='utf-8') as f:
+        print(f.read())
+
+    print('.........')
+    # 逐行读取文件
+    with open('1.txt', mode='r') as f:
+        for line in f:
+            print(line, end='')
+            time.sleep(0.5)
+    print('\n***********')
+
+    # 读取到一个list中
+    with open('1.txt') as f:
+        lines = f.readlines()
+        print('lines:', lines)
+
+
+if __name__ == '__main__':
+    main()

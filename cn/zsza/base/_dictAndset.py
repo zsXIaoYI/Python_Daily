6358b1dc442324366f3f50e745f5ef3e25a73494
zhangsong01 <zhangsong01@missfresh.cn>
2019-06-26 14:22:14 +0800

    新增一些code

--- cn/zsza/base/_dictAndset.py
+++ cn/zsza/base/_dictAndset.py
@@ -66,3 +66,10 @@ print(s1)
 
 dd = {'one': "This is one", 2: "This is two"}
 print(dd)
+
+DIAL_CODES = [(86, 'China'), (91, 'India'), (36, 'Brazil')]
+country_code = {country: code for code, country in DIAL_CODES}
+print('country_code:', country_code)
+
+new_country_code = {code: country.upper() for country, code in country_code.items() if code > 50}
+print('new_country_code:', new_country_code)

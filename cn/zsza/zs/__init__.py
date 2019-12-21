655c08d9c8c74fc86cf874fb2717dae13280caa3
zhangsong01 <zhangsong01@missfresh.cn>
2019-12-21 20:27:36 +0800

    新增zs包

---
+++ cn/zsza/zs/__init__.py
@@ -0,0 +1,5 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time: 2019/12/21 12:16
+# @File: __init__.py.py
+# __author__ = 'zs'
---
+++ cn/zsza/zs/init_test.py
@@ -0,0 +1,11 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time: 2019/12/21 12:16
+# @File: init_test.py
+# __author__ = 'zs'
+
+
+from cn.zsza.tool.test import print_info
+
+# 当执行该方法时,cn.zsza.tool、cn.zsza、cn包下的__init__.py会自动执行
+print_info()

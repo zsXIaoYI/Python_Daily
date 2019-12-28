53c5d36ff93bb365e88fa991e21640eb2950c987
zhangsong01 <zhangsong01@missfresh.cn>
2019-12-28 20:09:13 +0800

    添加json其他操作

--- cn/zsza/io/_json.py
+++ cn/zsza/io/_json.py
@@ -19,3 +19,14 @@ print('json_str:', json_str)
 # 2、将字符串转换成Python中的dict
 py_dict = json.loads(json_str)
 print('py_dict:', py_dict)
+
+
+# 3、dump函数,将Python数据dict写入到json文件
+with open('user_info.json', 'w', encoding='utf-8') as f:
+    json.dump(user_info_dict, f)
+
+
+# 4、load函数，加载json文件，并将文件数据转换成Python数据结构
+with open('user_info.json', 'r', encoding='utf-8') as f:
+    temp_user_info_dict = json.load(f)
+    print(temp_user_info_dict)
